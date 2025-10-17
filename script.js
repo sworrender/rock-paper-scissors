fc316a9684adf45370b6e66c77069858e637a40c
Shelby Orrender <shelbyorrender@gmail.com>
2025-10-16 20:35:40 -0400

    added rounds and a game over as well as reset

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-const rock = document.querySelector("#rock")
-const paper = document.querySelector("#paper")
-const scissors = document.querySelector("#scissors")
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 let human_answer = "";
 let computer_answer = null;
 let computer_guess = "";
 
-
-rock.addEventListener("click", e => {
-    human_answer = "rock";
-    computer_answer = Math.random();
-    computer_guess = guess(computer_answer);
-    document.getElementById("computer").textContent = `Computer: ${computer_guess}`;
-    document.getElementById("human").textContent = `Human Answer: ${human_answer}`;
-    who_wins(computer_guess, human_answer);
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", e => {
+    window.location.reload()
 })
-
-paper.addEventListener("click", e => {
-    human_answer = "paper";
-    computer_answer = Math.random();
-    computer_guess = guess(computer_answer);
-    document.getElementById("computer").textContent = `Computer: ${computer_guess}`;
-    document.getElementById("human").textContent = `Human Answer: ${human_answer}`;
-    who_wins(computer_guess, human_answer);
-})
-
-scissors.addEventListener("click", e => {
-    human_answer = "scissors";
-    computer_answer = Math.random();
-    computer_guess = guess(computer_answer);
-    document.getElementById("computer").textContent = `Computer: ${computer_guess}`;
-    document.getElementById("human").textContent = `Human Answer: ${human_answer}`;
-    who_wins(computer_guess, human_answer);
-})
-
 
 function guess(computer_answer) {
     if (computer_answer < .3333) {
@@ -44,41 +20,102 @@ function guess(computer_answer) {
     }
 }
 
-function who_wins(computer_guess,human_answer) {
-    if (computer_guess == "paper" && human_answer == "paper") {
-        document.getElementById("message").textContent = "Draw";
-        document.getElementById("message").style.color = "black";
+
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    playRound();
+    function whoWon() {
+        if (humanScore > computerScore) {
+            document.getElementById("game-status").textContent = "Game Over: You Win";
+            document.getElementById("game-status").style.color = "green";
+        } else {
+            document.getElementById("game-status").textContent = "Game Over: You Lose";
+            document.getElementById("game-status").style.color = "red";
+        }
     }
-    else if (computer_guess == "paper" && human_answer == "scissors") {
-        document.getElementById("message").textContent = "You Win";
-        document.getElementById("message").style.color = "green";
+    
+    function gameOver(computer_guess,human_answer) {
+        if (humanScore < 5 && computerScore < 5) {
+            who_wins(computer_guess,human_answer);
+            document.getElementById("computer").textContent = `Computer: ${computer_guess}`;
+            document.getElementById("human").textContent = `Human Answer: ${human_answer}`;
+            document.getElementById("computer-score").textContent = `Computer: ${computerScore}`;
+            document.getElementById("human-score").textContent = `Human Answer: ${humanScore}`;
+            document.getElementById("game-status").textContent = "Game in Progress";
+        } else {
+            whoWon();
+        }
     }
-    else if (computer_guess == "paper" && human_answer == "rock") {
-        document.getElementById("message").textContent = "You Lose";
-        document.getElementById("message").style.color = "red";
+
+    function playRound() {
+        rock.addEventListener("click", e => {
+        human_answer = "rock";
+        computer_answer = Math.random();
+        computer_guess = guess(computer_answer);
+        gameOver(computer_guess,human_answer);
+        })
+
+        paper.addEventListener("click", e => {
+        human_answer = "paper";
+        computer_answer = Math.random();
+        computer_guess = guess(computer_answer);
+        gameOver(computer_guess,human_answer);
+        })
+
+        scissors.addEventListener("click", e => {
+        human_answer = "scissors";
+        computer_answer = Math.random();
+        computer_guess = guess(computer_answer);
+        gameOver(computer_guess,human_answer);
+        })
     }
-    else if (computer_guess == "rock" && human_answer == "rock") {
-        document.getElementById("message").textContent = "Draw";
-        document.getElementById("message").style.color = "black";
-    }
-    else if (computer_guess == "rock" && human_answer == "paper") {
-        document.getElementById("message").textContent = "You Win";
-        document.getElementById("message").style.color = "green";
-    }
-    else if (computer_guess == "rock" && human_answer == "scissors") {
-        document.getElementById("message").textContent = "You Lose";
-        document.getElementById("message").style.color = "red";
-    }
-    else if (computer_guess == "scissors" && human_answer == "paper") {
-        document.getElementById("message").textContent = "You Lose";
-        document.getElementById("message").style.color = "red";
-    }
-    else if (computer_guess == "scissors" && human_answer == "rock") {
-        document.getElementById("message").textContent = "You Win";
-        document.getElementById("message").style.color = "green";
-    }
-    else if (computer_guess == "scissors" && human_answer == "scissors") {
-        document.getElementById("message").textContent = "Draw";
-        document.getElementById("message").style.color = "black";
+
+    function who_wins(computer_guess,human_answer) {
+        if (computer_guess == "paper" && human_answer == "paper") {
+            document.getElementById("message").textContent = "Draw";
+            document.getElementById("message").style.color = "black";
+        }
+        else if (computer_guess == "paper" && human_answer == "scissors") {
+            document.getElementById("message").textContent = "You Won This Round";
+            document.getElementById("message").style.color = "green";
+            humanScore++
+        }
+        else if (computer_guess == "paper" && human_answer == "rock") {
+            document.getElementById("message").textContent = "You Lost This Round";
+            document.getElementById("message").style.color = "red";
+            computerScore++
+        }
+        else if (computer_guess == "rock" && human_answer == "rock") {
+            document.getElementById("message").textContent = "Draw";
+            document.getElementById("message").style.color = "black";
+        }
+        else if (computer_guess == "rock" && human_answer == "paper") {
+            document.getElementById("message").textContent = "You Won This Round";
+            document.getElementById("message").style.color = "green";
+            humanScore++
+        }
+        else if (computer_guess == "rock" && human_answer == "scissors") {
+            document.getElementById("message").textContent = "You Lost This Round";
+            document.getElementById("message").style.color = "red";
+            computerScore++
+        }
+        else if (computer_guess == "scissors" && human_answer == "paper") {
+            document.getElementById("message").textContent = "You Lost This Round";
+            document.getElementById("message").style.color = "red";
+            computerScore++
+        }
+        else if (computer_guess == "scissors" && human_answer == "rock") {
+            document.getElementById("message").textContent = "You Won This Round";
+            document.getElementById("message").style.color = "green";
+            humanScore++
+        }
+        else if (computer_guess == "scissors" && human_answer == "scissors") {
+            document.getElementById("message").textContent = "Draw";
+            document.getElementById("message").style.color = "black";
+        }
     }
 }
+
+playGame();
